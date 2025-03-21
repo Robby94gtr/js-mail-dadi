5ddd501f51a9f4157fb9389a2f2ebab762abd5da
Robby94gtr <robertogramazio94@gmail.com>
2025-03-21 19:18:47 +0100

    [UPD] Creato generatore di numeri casuale da 1 a 6 per l'utente

--- js/script.js
+++ js/script.js
@@ -32,7 +32,15 @@ else{
     console.log("Invalid Email");
 }
 
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
 
+// Creo generatore di numeri casuale da 1 a 6 per l'utente e stampo il messaggio
+
+let userNumber=Math.floor(Math.random() * 6) + 1;
+
+console.log(userNumber + " Utente") 
 
 
 
